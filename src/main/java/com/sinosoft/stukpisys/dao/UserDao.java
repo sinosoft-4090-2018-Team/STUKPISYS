d45f662617ab2a019ec393dee70091bd2322db40
xuenansea <isxuenan@163.com>
2018-03-27 09:24:39 +0800

    123

--- src/main/java/com/sinosoft/stukpisys/dao/UserDao.java
+++ src/main/java/com/sinosoft/stukpisys/dao/UserDao.java
@@ -3,9 +3,13 @@ package com.sinosoft.stukpisys.dao;
 import com.sinosoft.stukpisys.entity.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 @Mapper
+@Repository
 public interface UserDao{
 
     @Insert("insert into user(admin,createTime) value(#{admin},#{createTime})")
     int insert(User user);
+
 }
