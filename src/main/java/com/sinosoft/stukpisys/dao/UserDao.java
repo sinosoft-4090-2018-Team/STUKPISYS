44602fd8ec28e70527700c51a2299666d3a420ee
Kangjun327 <919903363@qq.com>
2018-03-27 09:54:44 +0800

    实体类创建--kangJun

--- src/main/java/com/sinosoft/stukpisys/dao/UserDao.java
+++ src/main/java/com/sinosoft/stukpisys/dao/UserDao.java
@@ -1,12 +1,16 @@
 package com.sinosoft.stukpisys.dao;
 
 import com.sinosoft.stukpisys.entity.User;
+import com.sinosoft.stukpisys.entity.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
 @Mapper
+@Repository
 public interface UserDao{
 
     User getByName(String name);
@@ -26,4 +30,7 @@ public interface UserDao{
 
 
     void insert(User user);
+
+    @Select("select * from user_info")
+    List<UserInfo> getAllInfo();
 }
