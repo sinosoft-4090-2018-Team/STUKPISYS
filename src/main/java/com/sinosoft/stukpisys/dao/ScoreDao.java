f14e0310af984f5cb9f9dcece7256f736bb95227
xuenansea <isxuenan@163.com>
2018-03-29 11:00:30 +0800

    xn:获取各阶段的学分详细情况

--- src/main/java/com/sinosoft/stukpisys/dao/ScoreDao.java
+++ src/main/java/com/sinosoft/stukpisys/dao/ScoreDao.java
@@ -83,5 +83,18 @@ public interface ScoreDao {
             "\t\t\tm.user_id")
     List<List<Object>>  getUserAllScoreByTime(java.sql.Date enter_time);
 
+    /*
+    查看某同学某阶段各成绩的学分情况，通过员工id和阶段下标
+    默认Stage 情况：第一阶段各小考核的stage值为0；
+    第一阶段总考核成绩stage值为1；第二阶段总考核成绩stage值为2；第三阶段总考核成绩stage值为3
+     */
+    @Select("SELECT  l.label_name, \n" +
+            "IF(l.stage=0 ,IF(v.value_int>=90,5,\n" +
+            "IF(v.value_string='合格',3,0)),v.value_int)\n" +
+            "detailScore  \n" +
+            "FROM score_label l JOIN score_value v\n" +
+            "ON l.label_index=v.label_index\n" +
+            "where user_id=#{0} AND l.stage=#{1}")
+     List<List<Object>> getScoreFromFirstStageByUser_id(int userId,int stage );
 
 }
