b7a1faa81601023b2a8642233cc855d141952950
zrtzrt <519539110@qq.com>
2018-04-08 18:41:43 +0800

    前端

--- src/main/resources/project/js/head.js
+++ src/main/resources/project/js/head.js
@@ -6,8 +6,14 @@
 		.then(html=> {
 			document.getElementById("header").innerHTML = html;
 			$(".info-select").on("click","li",function(){
-				$(this).addClass("bg-color1").siblings().removeClass("bg-color1");
-			});
+            	$(this).addClass("bg-color1").siblings().removeClass("bg-color1");
+            });
+            // $(".sta1").on("click",function(){
+				// $(this).addClass("bg-color1").next().removeClass("bg-colocr1");
+            // });
+            // $(".sta2").on("click",function(){
+            //     $(this).addClass("bg-color1").prev().removeClass("bg-colocr1");
+            // });
 			$(".img1").on("click",function(){
 				$(".reset").toggleClass("none2");
 			});
