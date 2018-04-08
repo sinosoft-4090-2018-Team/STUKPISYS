84acbf4eb02554cf7eeb9a1618fd43e32fb7923c
zrtzrt <519539110@qq.com>
2018-04-08 18:32:52 +0800

    前端

--- src/main/resources/project/js/trainee.js
+++ src/main/resources/project/js/trainee.js
@@ -2,7 +2,13 @@
 let trainee = new Vue({
     el: '#trainee',
     data: {
-        name: ''
+        name: '',
+        state:'正常',
+        locat:'江苏南京',
+        job:'开发',
+        birth:'',
+        dept:'',
+
         //todo
     },
     mounted: function () {
@@ -13,6 +19,36 @@ let trainee = new Vue({
             this.name = decodeURI(window.location.href.split('=')[1]);
             console.log(this.name);
             this.getScore();
+            this.getInfo();
+        },
+        getInfo(){
+            this.$http.get('/trainee/info?name='+this.name).then((response) => {
+                console.log(response);
+                let data = response.data;
+                this.birth=data.userInfo.birth;
+                this.dept=data.userInfo.dept;
+                this.email=data.userInfo.email;
+                this.enterTime=data.userInfo.enterTime;
+                this.sex=data.userInfo.gender==0?'男':'女';
+                this.hrName=data.userInfo.hrName;
+                this.job=data.userInfo.job;
+                this.nativePlace=data.userInfo.nativePlace;
+                this.phone=data.userInfo.phone;
+                this.state=data.userInfo.state;
+                this.graduationTime=data.education.graduationTime;
+                this.highestEducate=data.education.highestEducate;
+                this.major=data.education.major;
+                this.schoolName=data.education.schoolName;
+            });
+        },
+        getJudge(){
+            this.$http.get('/trainee/judge?name='+this.name).then((response) => {
+                console.log(response);
+                let data = response.data;
+                //todo
+            }).catch(function (error) {
+                alert("载入信息出错，"+error)
+            });
         },
         getScore(){
             this.$http.get('/trainee/score?name='+this.name).then((response) => {
